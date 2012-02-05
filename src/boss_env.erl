17bacf1dbecd6966e24e20ef6e4ec8f95bc9d7a3
Niclas Axelsson <niclas@burbas.se>
2012-02-05 21:29:11 +0100

    Add missing boss_env.erl

---
+++ src/boss_env.erl
@@ -0,0 +1,33 @@
+-module(boss_env).
+
+-export([boss_env/0, setup_boss_env/0, get_env/2, get_env/3]).
+-export([is_master_node/0, is_developing_app/1]).
+
+boss_env() ->
+    case get(boss_environment) of
+        undefined -> setup_boss_env();
+        Val -> Val
+    end.
+
+setup_boss_env() ->	
+    case boss_load:module_is_loaded(reloader) of
+        true -> put(boss_environment, development), development;
+        false -> put(boss_environment, production), production
+    end.			
+
+get_env(App, Key, Default) when is_atom(App), is_atom(Key) ->
+    case application:get_env(App, Key) of
+        {ok, Val} -> Val;
+        _ -> Default
+    end.
+
+get_env(Key, Default) when is_atom(Key) ->
+    get_env(boss, Key, Default).
+
+is_master_node() ->
+    boss_env:get_env(master_node, erlang:node()) =:= erlang:node().
+
+is_developing_app(AppName) ->
+    BossEnv = boss_env:boss_env(),
+    DevelopingApp = boss_env:get_env(developing_app, undefined),
+    AppName =:= DevelopingApp andalso BossEnv =:= development.
