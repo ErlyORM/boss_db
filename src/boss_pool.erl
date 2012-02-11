f8836ace09136920e6d395239970acd4101751cf
Evan Miller <emmiller+github@gmail.com>
2012-02-10 20:21:42 -0600

    Forgot to add new boss_pool.erl

---
+++ src/boss_pool.erl
@@ -0,0 +1,14 @@
+-module(boss_pool).
+-export([call/2, call/3]).
+
+call(Pool, Msg) ->
+    Worker = poolboy:checkout(Pool),
+    Reply = gen_server:call(Worker, Msg),
+    poolboy:checkin(Pool, Worker),
+    Reply.
+
+call(Pool, Msg, Timeout) ->
+    Worker = poolboy:checkout(Pool),
+    Reply = gen_server:call(Worker, Msg, Timeout),
+    poolboy:checkin(Pool, Worker),
+    Reply.
