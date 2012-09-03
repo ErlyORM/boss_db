4767a791856b4cea1c3a8e6e137ef60cc27710c4
Evan Miller <emmiller@gmail.com>
2012-09-03 10:35:10 -0500

    Support case-insensitive matches in Mongo (maybe)

--- src/db_adapters/boss_db_adapter_mongodb.erl
+++ src/db_adapters/boss_db_adapter_mongodb.erl
@@ -200,8 +200,12 @@ build_conditions1([{Key, Operator, Value}|Rest], Acc) ->
 %    ?LOG("Key, Operator, Value", {Key, Operator, Value}),
 
     Condition = case {Operator, Value} of 
+        {'not_matches', "*"++Value} ->
+            [{Key, {'$not', {regex, list_to_binary(Value), <<"i">>}}}];
         {'not_matches', Value} ->
             [{Key, {'$not', {regex, list_to_binary(Value), <<"">>}}}];
+        {'matches', "*"++Value} ->
+            [{Key, {regex, list_to_binary(Value), <<"i">>}}];
         {'matches', Value} ->
             [{Key, {regex, list_to_binary(Value), <<"">>}}];
         {'contains', Value} ->
