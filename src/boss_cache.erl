3f04cafe040514c74054d51571bc33049edabf76
cuongth <bronzeboyvn@gmail.com>
2013-06-22 23:07:34 +0700

    Replace boss_cache_controller pool with ETS table

--- src/boss_cache.erl
+++ src/boss_cache.erl
@@ -2,8 +2,15 @@
 -export([start/0, start/1]).
 -export([stop/0]).
 -export([get/2, set/4, delete/2]).
+-export([terminate/1]).
 
--define(POOLNAME, boss_cache_pool).
+%% Maintain the adapter and connection state in a singleton ETS table entry
+%% rather than a pool of boss_cache_controller processes.
+-define(BOSS_CACHE_TABLE, boss_cache).
+-record(state, {
+          adapter,
+          connection
+         }).
 
 start() ->
     Adapter = boss_cache_adapter_memcached_bin,
@@ -13,16 +20,53 @@ start(Options) ->
     AdapterName = proplists:get_value(adapter, Options, memcached_bin),
     Adapter = list_to_atom(lists:concat(["boss_cache_adapter_", AdapterName])),
     Adapter:start(Options),
-    boss_cache_sup:start_link(Options).
+    {ok, Conn} = Adapter:init(Options),
+    State = #state{ adapter = Adapter, connection = Conn },
+    setup_table(),
+    set_state(State),
+    {ok, State}.
 
 stop() ->
+    teardown_table(),
     ok.
 
+%% Create a new empty table if none exists yet.
+setup_table() ->
+    case ets:info(?BOSS_CACHE_TABLE) of
+        undefined ->
+	    ets:new(?BOSS_CACHE_TABLE, [set, public, named_table, {read_concurrency, true}]);
+        _X ->
+	    ets:delete_all_objects(?BOSS_CACHE_TABLE)
+    end.
+
+%% Delete the table if one exists.
+teardown_table() ->
+    case ets:info(?BOSS_CACHE_TABLE) of
+        undefined ->
+            ok;
+        _X ->
+            ets:delete(?BOSS_CACHE_TABLE)
+    end.
+
+get_state() ->
+    [{state, State}] = ets:lookup(?BOSS_CACHE_TABLE, state),
+    State.
+
+set_state(State) ->
+    ets:insert(?BOSS_CACHE_TABLE, {state, State}).
+
 set(Prefix, Key, Val, TTL) ->
-    boss_pool:call(?POOLNAME, {set, Prefix, Key, Val, TTL}).
+    #state{ adapter=Adapter, connection=Conn } = get_state(),
+    Adapter:set(Conn, Prefix, Key, Val, TTL).
 
 get(Prefix, Key) ->
-    boss_pool:call(?POOLNAME, {get, Prefix, Key}).
+    #state{ adapter=Adapter, connection=Conn } = get_state(),
+    Adapter:get(Conn, Prefix, Key).
 
 delete(Prefix, Key) ->
-    boss_pool:call(?POOLNAME, {delete, Prefix, Key}).
+    #state{ adapter=Adapter, connection=Conn } = get_state(),
+    Adapter:delete(Conn, Prefix, Key).
+
+terminate(_Reason) ->
+    #state{ adapter=Adapter, connection=Conn } = get_state(),
+    Adapter:terminate(Conn).
