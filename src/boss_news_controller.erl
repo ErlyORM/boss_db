79aac238c6365779fabd6a47a7b7c510035c247d
Zachary Kessin <zkessin@gmail.com>
2013-12-22 13:56:35 +0200

    first refactors

--- src/boss_news_controller.erl
+++ src/boss_news_controller.erl
@@ -2,27 +2,51 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, start_link/1]).
-
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
-
+-ifdef(TEST).
+-compile(export_all).
+-endif.
 -record(state, {
-        watch_dict = dict:new(),
-        ttl_tree = gb_trees:empty(),
+        watch_dict		= dict:new(),
+        ttl_tree		= gb_trees:empty() ::gb_tree(),
 
-        set_watchers = dict:new(), 
-        id_watchers = dict:new(),
+        set_watchers		= dict:new(), 
+        id_watchers		= dict:new(),
 
-        set_attr_watchers = dict:new(),
-        id_attr_watchers = dict:new(),
-        watch_counter = 0}).
+        set_attr_watchers	= dict:new(),
+        id_attr_watchers	= dict:new(),
+        watch_counter		= 0}).
 
 -record(watch, {
-        watch_list = [],
+        watch_list		= [],
         callback,
         user_info,
         exp_time,
         ttl}).
+
+-type news_callback()	:: fun((event(),event_info()) ->any()) | fun((event(),event_info(), user_info())-> any()).
+-type event()		:: any().
+-type event_info()	:: any().
+-type user_info()	:: any().
+
+-spec start_link()	-> 'ignore' | {'error',_} | {'ok',pid()}.
+-spec start_link(_)	-> 'ignore' | {'error',_} | {'ok',pid()}.
+-spec init(_) -> {'ok',#state{watch_dict::dict(),ttl_tree::gb_tree(),set_watchers::dict(),id_watchers::dict(),set_attr_watchers::dict(),id_attr_watchers::dict(),watch_counter::0}}.
+-spec handle_call('dump' | 'reset' | {'cancel_watch',_} | {'extend_watch',_} | {'created',binary() | maybe_improper_list(binary() | maybe_improper_list(any(),binary() | []) | char(),binary() | []),_} | {'deleted',binary() | maybe_improper_list(binary() | maybe_improper_list(any(),binary() | []) | char(),binary() | []),_} | {'updated',binary() | maybe_improper_list(binary() | maybe_improper_list(any(),binary() | []) | char(),binary() | []),_,_} | {'watch',binary() | maybe_improper_list(binary() | maybe_improper_list(any(),binary() | []) | char(),binary() | []),_,_,number()} | {'set_watch',_,binary() | maybe_improper_list(binary() | maybe_improper_list(any(),binary() | []) | char(),binary() | []),_,_,number()},_,_) -> {'reply',_,#state{ttl_tree::gb_tree()}}.
+-spec handle_cast(_,_) -> {'noreply',_}.
+-spec terminate(_,_) -> 'ok'.
+-spec code_change(_,_,_) -> {'ok',_}.
+-spec handle_info(_,_) -> {'noreply',_}.
+-spec future_time(number()) -> number().
+-spec activate_record(binary() | maybe_improper_list(binary() | maybe_improper_list(any(),binary() | []) | char(),binary() | []),_) -> any().
+-spec prune_expired_entries(#state{ttl_tree::gb_tree()}) -> #state{ttl_tree::gb_tree()}.
+-spec execute_callback(news_callback(),event(),event_info(),user_info(),_) -> pid().
+-spec execute_fun(news_callback(),event(),event_info(),user_info()) -> any().
+
+-export([start_link/0, start_link/1]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+
 
 start_link() ->
     start_link([]).
@@ -38,8 +62,7 @@ handle_call(reset, _From, _State) ->
 handle_call(dump, _From, State0) ->
     State = prune_expired_entries(State0),
     {reply, State, State};
-handle_call({watch, TopicString, CallBack, UserInfo, TTL}, From, State0) ->
-    WatchId = State0#state.watch_counter,
+handle_call({watch, TopicString, CallBack, UserInfo, TTL}, From, State0 = #state{watch_counter= WatchId}) ->
     {reply, RetVal, State} = handle_call({set_watch, WatchId, TopicString, CallBack, UserInfo, TTL}, From, State0),
     case RetVal of
         ok ->
@@ -180,6 +203,7 @@ handle_call({deleted, Id, OldAttrs}, _From, State0) ->
         _ -> {ok, State}
     end,
     {reply, RetVal, State1};
+
 handle_call({updated, Id, OldAttrs, NewAttrs}, _From, State0) ->
     State = prune_expired_entries(State0),
     [Module | _IdNum] = re:split(Id, "-", [{return, list}, {parts, 2}]),
@@ -244,72 +268,69 @@ future_time(TTL) ->
     MegaSecs * 1000 * 1000 + Secs + TTL.
 
 activate_record(Id, Attrs) ->
-    [Module | _IdNum] = re:split(Id, "-", [{return, list}, {parts, 2}]),
-    Type = list_to_atom(Module),
-    DummyRecord = boss_record_lib:dummy_record(Type),
+    [Module | _IdNum]	= re:split(Id, "-", [{return, list}, {parts, 2}]),
+    Type		= list_to_existing_atom(Module),
+    DummyRecord		= boss_record_lib:dummy_record(Type),
     apply(Type, new, lists:map(fun
                 (id) -> Id;
                 (Key) -> proplists:get_value(Key, Attrs)
             end, DummyRecord:attribute_names())).
 
+
+
 prune_expired_entries(#state{ ttl_tree = Tree } = State) ->
     Now = future_time(0),
     {NewState, NewTree} = tiny_pq:prune_collect_old(fun(WatchId, StateAcc) ->
-                #watch{ watch_list = WatchList } = dict:fetch(WatchId, StateAcc#state.watch_dict),
-                NewState = lists:foldr(fun
-                        ({id, TopicString}, Acc) ->
-                            NewDict = case dict:fetch(TopicString, StateAcc#state.id_watchers) of
-                                [WatchId] ->
-                                    dict:erase(TopicString, StateAcc#state.id_watchers);
-                                Watchers ->
-                                    dict:store(TopicString, lists:delete(WatchId, Watchers), StateAcc#state.id_watchers)
-                            end,
-                            Acc#state{ id_watchers = NewDict };
-                        ({set, TopicString}, Acc) ->
-                            NewDict = case dict:fetch(TopicString, StateAcc#state.set_watchers) of
-                                [WatchId] ->
-                                    dict:erase(TopicString, StateAcc#state.set_watchers);
-                                Watchers ->
-                                    dict:store(TopicString, lists:delete(WatchId, Watchers), StateAcc#state.set_watchers)
-                            end,
-                            Acc#state{ set_watchers = NewDict };
-                        ({id_attr, Id, _Attr}, Acc) ->
-                            NewDict = case dict:fetch(Id, StateAcc#state.id_attr_watchers) of
-                                [WatchId] ->
-                                    dict:erase(Id, StateAcc#state.id_attr_watchers);
-                                Watchers ->
-                                    dict:store(Id, lists:delete(WatchId, Watchers), StateAcc#state.id_attr_watchers)
-                            end,
-                            Acc#state{ id_attr_watchers = NewDict };
-                        ({set_attr, Module, _Attr}, Acc) ->
-                            NewDict = case dict:fetch(Module, StateAcc#state.set_attr_watchers) of
-                                [WatchId] ->
-                                    dict:erase(Module, StateAcc#state.set_attr_watchers);
-                                Watchers ->
-                                    dict:store(Module, lists:delete(WatchId, Watchers), StateAcc#state.set_attr_watchers)
-                            end,
-                            Acc#state{ set_attr_watchers = NewDict };
-                        (_, Acc) ->
-                            Acc
-                    end, StateAcc, WatchList),
-                NewState#state{ watch_dict = dict:erase(WatchId, StateAcc#state.watch_dict) }
-        end, State, Tree, Now),
+							    #watch{ watch_list = WatchList } = dict:fetch(WatchId, StateAcc#state.watch_dict),
+							    NewState = process_news_state( WatchId, StateAcc, WatchList),
+							    NewState#state{ watch_dict = dict:erase(WatchId, StateAcc#state.watch_dict) }
+						    end, State, Tree, Now),
     NewState#state{ ttl_tree = NewTree }.
+
+process_news_state( WatchId, StateAcc, WatchList) ->
+    lists:foldr(fun
+		    ({id, TopicString}, Acc) ->
+			NewDict = process_dict(WatchId, StateAcc#state.id_watchers, TopicString),
+			Acc#state{id_watchers = NewDict};
+		    ({set, TopicString}, Acc) ->
+			NewDict = process_dict(WatchId, StateAcc#state.set_watchers, TopicString),
+			Acc#state{set_watchers = NewDict};
+		    ({id_attr, Id, _Attr}, Acc) ->
+			NewDict = process_dict(WatchId, StateAcc#state.id_attr_watchers, Id),
+			Acc#state{id_attr_watchers = NewDict};
+		    ({set_attr, Module, _Attr}, Acc) ->
+			NewDict = process_dict(WatchId, StateAcc#state.set_attr_watchers, Module),
+			Acc#state{set_attr_watchers = NewDict};
+		    (_, Acc) ->
+			Acc
+                end, StateAcc, WatchList).
+
+process_dict(WatchId, Dict, TopicString) ->
+    case dict:fetch(TopicString, Dict) of
+	[WatchId] ->
+	    dict:erase(TopicString, Dict);
+	Watchers ->
+	    dict:store(TopicString, lists:delete(WatchId, Watchers), Dict)
+    end.
 
 execute_callback(Fun, Event, EventInfo, UserInfo, WatchId) when is_function(Fun) ->
     erlang:spawn(fun() ->
-                Result = case proplists:get_value(arity, erlang:fun_info(Fun)) of
-                    2 ->
-                        Fun(Event, EventInfo);
-                    3 ->
-                        Fun(Event, EventInfo, UserInfo)
-                end,
-                case Result of
-                    {ok, cancel_watch} -> 
-                        boss_news:cancel_watch(WatchId);
-                    {ok, extend_watch} -> 
-                        boss_news:extend_watch(WatchId);
-                    _ ->
+			 Result = execute_fun(Fun, Event, EventInfo, UserInfo),
+			 case Result of
+			     {ok, cancel_watch} -> 
+				 boss_news:cancel_watch(WatchId);
+			     {ok, extend_watch} -> 
+				 boss_news:extend_watch(WatchId);
+			     _ ->
                         ok
-                end
-        end).
+			 end
+		 end).
+
+
+execute_fun(Fun, Event, EventInfo, UserInfo) ->
+    case proplists:get_value(arity, erlang:fun_info(Fun)) of
+	2 ->
+	    Fun(Event, EventInfo);
+	3 ->
+	    Fun(Event, EventInfo, UserInfo)
+    end.
