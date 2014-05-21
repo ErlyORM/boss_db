79c618930c3b3366ab7476450aca2a375a45698d
David N. Welton <davidw@dedasys.com>
2014-05-21 15:28:02 +0200

    Use current application, rather than boss.

--- src/boss_db_sup.erl
+++ src/boss_db_sup.erl
@@ -14,9 +14,12 @@ start_link(StartArgs) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, StartArgs).
 
 init(StartArgs) ->
+
+    AppName = application:get_application(),
+
     Args = [{name, {local, boss_db_pool}},
             {worker_module, boss_db_controller},
-            {size, application:get_env(boss, db_pool_size, 5)},
-            {max_overflow, application:get_env(boss, db_pool_max_overflow, 10)}|StartArgs],
+            {size, application:get_env(AppName, db_pool_size, 5)},
+            {max_overflow, application:get_env(AppName, db_pool_max_overflow, 10)}|StartArgs],
     PoolSpec = {db_controller, {poolboy, start_link, [Args]}, permanent, 2000, worker, [poolboy]},
     {ok, {{one_for_one, 10, 10}, [PoolSpec]}}.
