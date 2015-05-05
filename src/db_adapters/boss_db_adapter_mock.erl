2dbe511b6646b93c503b81cde3d0e48855b9b00b
himangshu <himangshu@carcredible.com>
2015-05-05 19:26:36 +0530

    Added backward_compatibility for all finds
    Signed-off-by: himangshu <himangshu@carcredible.com>

--- src/db_adapters/boss_db_adapter_mock.erl
+++ src/db_adapters/boss_db_adapter_mock.erl
@@ -2,7 +2,7 @@
 -module(boss_db_adapter_mock).
 -behaviour(boss_db_adapter).
 -export([init/1, terminate/1, start/1, stop/0]).
--export([find/2, find/7, count/3, counter/2, incr/3, delete/2, save_record/2]).
+-export([find/2, find/7, find/8,count/3, counter/2, incr/3, delete/2, save_record/2]).
 -export([push/2, pop/2, dump/1, transaction/2]).
 
 start(Options) ->
@@ -24,6 +24,11 @@ terminate(_) ->
 
 find(_, Id) ->
     gen_server:call({global, boss_db_mock}, {find, Id}).
+
+find(Pid, Type, Conditions, Max, Skip, Sort, SortOrder, _) when is_atom(Type), is_list(Conditions),
+    is_integer(Max) orelse Max =:= all, is_integer(Skip),
+    is_atom(Sort), is_atom(SortOrder) ->
+    find(Pid, Type, Conditions, Max, Skip, Sort, SortOrder).
 
 find(_, Type, Conditions, Max, Skip, SortBy, SortOrder) ->
     gen_server:call({global, boss_db_mock}, {find, Type, Conditions, Max, Skip, SortBy, SortOrder}).
